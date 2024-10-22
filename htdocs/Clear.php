12c97b90332e6cbc8619c3371cfac82c7b750fc8
Студент 13 Кабинет 409 <409-13@coopteh.local>
2024-10-22 16:34:00 +0700

    first commit

--- htdocs/Clear.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-$_SESSION['basket'] = [];
-header('Location: https://localhost/orders');
--- htdocs/test_product.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Routers\Router;
-
-$obj = new Router();
-echo $obj->route('https://mysite.ru/products') ."\n";      // должен быть вызван метод getAll() класса Product'
-echo $obj->route('https://mysite.ru/products/1') ."\n";      // должен быть вызван метод get(1) класса Product
-echo $obj->route('https://mysite.ru/products/100') ."\n";      // 404
